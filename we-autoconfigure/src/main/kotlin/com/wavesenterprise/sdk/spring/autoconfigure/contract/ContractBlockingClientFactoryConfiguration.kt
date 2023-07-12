3cdb654ca7ed2cf8db45291376e98b7664f7970a
dgeorgiev <dgeorgiev@web3tech.ru>
2023-07-12 14:24:21 +0300

    [WTCH-116] Update contract sdk version

--- we-autoconfigure/src/main/kotlin/com/wavesenterprise/sdk/spring/autoconfigure/contract/ContractBlockingClientFactoryConfiguration.kt
+++ we-autoconfigure/src/main/kotlin/com/wavesenterprise/sdk/spring/autoconfigure/contract/ContractBlockingClientFactoryConfiguration.kt
@@ -1,6 +1,9 @@
 package com.wavesenterprise.sdk.spring.autoconfigure.contract
 
+import com.wavesenterprise.sdk.contract.client.invocation.ContractLocalValidationExecutor
 import com.wavesenterprise.sdk.contract.core.converter.factory.ConverterFactory
+import com.wavesenterprise.sdk.contract.core.state.LocalValidationContextManager
+import com.wavesenterprise.sdk.contract.core.state.ThreadLocalLocalValidationContextManager
 import com.wavesenterprise.sdk.node.client.blocking.node.NodeBlockingServiceFactory
 import com.wavesenterprise.sdk.node.client.blocking.tx.TxService
 import com.wavesenterprise.sdk.tx.signer.node.factory.TxServiceTxSignerFactory
@@ -21,11 +24,21 @@ class ContractBlockingClientFactoryConfiguration {
     )
 
     @Bean
+    fun localValidationContextManager(): LocalValidationContextManager = ThreadLocalLocalValidationContextManager()
+
+    @Bean
+    fun contractLocalValidationExecutor(
+        localValidationContextManager: LocalValidationContextManager,
+    ): ContractLocalValidationExecutor = ContractLocalValidationExecutor(localValidationContextManager)
+
+    @Bean
     fun enabledContractBeanRegistryPostProcessor(
         converterFactory: ConverterFactory,
         applicationContext: ApplicationContext,
+        localValidationContextManager: LocalValidationContextManager,
     ): ContractBlockingClientFactoryRegistryPostProcessor = ContractBlockingClientFactoryRegistryPostProcessor(
         converterFactory = converterFactory,
         applicationContext = applicationContext,
+        localValidationContextManager = localValidationContextManager,
     )
 }

0f0b744a7cb9d2feee69145532327c2e75ed910c
dgeorgiev <dgeorgiev@web3tech.ru>
2023-07-20 10:58:16 +0300

    [WTCH-160] Edit contract version customizer and update versions of sdk-libs

--- we-autoconfigure/src/main/kotlin/com/wavesenterprise/sdk/spring/autoconfigure/contract/customizer/ContractSignRequestContractVersionCustomizer.kt
+++ we-autoconfigure/src/main/kotlin/com/wavesenterprise/sdk/spring/autoconfigure/contract/customizer/ContractSignRequestContractVersionCustomizer.kt
@@ -4,19 +4,18 @@ import com.wavesenterprise.sdk.node.client.blocking.contract.ContractService
 import com.wavesenterprise.sdk.node.domain.contract.ContractId
 import com.wavesenterprise.sdk.node.domain.sign.builder.ContractSignRequestBuilder
 import com.wavesenterprise.sdk.spring.autoconfigure.contract.ContractSignRequestCustomizer
-import com.wavesenterprise.sdk.spring.autoconfigure.contract.properties.ContractsProperties
 
 class ContractSignRequestContractVersionCustomizer(
-    private val contractsProperties: ContractsProperties,
     private val contractService: ContractService,
 ) : ContractSignRequestCustomizer {
 
-    override fun customize(contractKey: String, contractSignRequestBuilder: ContractSignRequestBuilder) {
-        val config = requireNotNull(contractsProperties.config[contractKey]) {
-            errorMessage("the configuration is not found for contract: $contractKey")
-        }
-        config.contractId?.let { contractId ->
-            val contractInfo = contractService.getContractInfo(ContractId.fromBase58(contractId)).orElseThrow {
+    override fun customize(
+        contractKey: String,
+        contractId: ContractId?,
+        contractSignRequestBuilder: ContractSignRequestBuilder,
+    ) {
+        contractId?.let {
+            val contractInfo = contractService.getContractInfo(contractId).orElseThrow {
                 IllegalArgumentException(
                     errorMessage("contract info not found by id: $contractId")
                 )

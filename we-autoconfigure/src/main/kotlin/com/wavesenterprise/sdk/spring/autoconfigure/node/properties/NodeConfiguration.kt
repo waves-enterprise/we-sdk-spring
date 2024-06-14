8460a46f317b8d86972f4b364e69aa953d09d2f3
dgeorgiev <dgeorgiev@web3tech.ru>
2024-06-14 15:41:04 +0000

    [WTCH-328] Add default bean of SenderAddressProvider

--- we-autoconfigure/src/main/kotlin/com/wavesenterprise/sdk/spring/autoconfigure/node/properties/NodeConfiguration.kt
+++ we-autoconfigure/src/main/kotlin/com/wavesenterprise/sdk/spring/autoconfigure/node/properties/NodeConfiguration.kt
@@ -1,18 +1,37 @@
 package com.wavesenterprise.sdk.spring.autoconfigure.node.properties
 
+import com.wavesenterprise.sdk.node.client.blocking.credentials.DefaultNodeCredentialsProvider
+import com.wavesenterprise.sdk.node.client.blocking.credentials.NodeCredentialsProvider
+import com.wavesenterprise.sdk.node.domain.Address
+import com.wavesenterprise.sdk.node.domain.Password
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableConfigurationProperties(
+    NodeCredentialsProperties::class,
+)
 @ConditionalOnProperty(value = ["node.legacy-mode"], havingValue = "false", matchIfMissing = true)
-class NodePropertiesConfiguration {
+class NodeConfiguration {
 
     @Bean
     @ConfigurationProperties(prefix = "node")
     fun nodeConfigurationProperties() = NodeConfigurationProperties()
+
+    @Bean
+    @ConditionalOnMissingBean
+    fun nodeCredentialsProvider(
+        nodeCredentialsProperties: NodeCredentialsProperties,
+    ): NodeCredentialsProvider =
+        DefaultNodeCredentialsProvider(
+            credentialsMap = nodeCredentialsProperties.addresses.map { (address, password) ->
+                Address.fromBase58(address) to password?.let { Password(password) }
+            }.toMap()
+        )
 
     @Bean
     @ConditionalOnMissingBean
